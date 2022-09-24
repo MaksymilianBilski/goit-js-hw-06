ef76cd31f1beb79e6195af5d4b8438ad0b258a79
MaksymilianBilski <biger9@o2.pl>
2022-09-24 20:00:50 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const button = document.querySelector(`button[type="button"]`);
+const body = document.querySelector("body");
+const text = document.querySelector(".color");
+console.log(text.textContent);
+
+button.addEventListener("click", () => {
+  text.textContent = body.style.backgroundColor = getRandomHexColor();
+});
