1f9434ea68dca951bfb67d3f02495f179d061e69
MaksymilianBilski <biger9@o2.pl>
2022-09-25 13:40:12 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,23 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const box = document.querySelector("#boxes");
+const buttonAdd = document.querySelector(`button[data-create]`);
+const buttonSub = document.querySelector(`button[data-destroy]`);
+const input = document.querySelector(`input[type="number"]`);
+
+const createBoxes = (amount) => {
+  amount = input.value;
+  for (let i = 0; i <= amount - 1; i++) {
+    const box2 = document.createElement("div");
+    const actualWidth = (box2.style.width = "30px");
+    const actualHeight = (box2.style.height = "30px");
+    box2.style.backgroundColor = `${getRandomHexColor()}`;
+    box.append(box2);
+    // const width = Number.parseFloat(actualWidth);
+    // const height = Number.parseFloat(actualHeight);
+  }
+};
+buttonAdd.addEventListener("click", createBoxes);
+
+const destroyBoxes = (amount) => {};
