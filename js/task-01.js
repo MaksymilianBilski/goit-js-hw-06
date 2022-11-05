0d0162221a1d2fc93c18d12c9eb3452d052ea80e
MaksymilianBilski <biger9@o2.pl>
2022-11-05 11:46:42 +0100

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -10,15 +10,3 @@ for (const it of items) {
   console.log("");
 }
 
-//2 SPOSÓB
-// const category = document.querySelector("#categories");
-// const child = category.children;
-// console.log("2 sposób: ");
-// console.log("Numbr of categories:" + items.length);
-//3 SPOSÓB
-// let number = 0;
-// for (const cat of child) {
-//   number += 1;
-// }
-// console.log("3 sposób: ");
-// console.log("Number of categories:" + number);
